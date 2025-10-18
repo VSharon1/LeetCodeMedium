231268ac060525b0c0b735e47abe694955715a54
VSharon1 <vs1.github@pm.me>
2025-10-18 07:52:56 +0200

    739 - done

---
+++ 739-daily-temperatures.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        answer = [0] * len(temperatures)
+        stack = []
+
+        for i, t in enumerate(temperatures):
+            while stack and t > stack[-1][0]:
+                stackT, stackI = stack.pop()
+                answer[stackI] = i - stackI
+
+            stack.append((t, i))
+
+        return answer
